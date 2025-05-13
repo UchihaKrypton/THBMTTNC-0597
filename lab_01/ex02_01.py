93c54747981ad869ab4d49263dc9eadfe90737a0
UchihaKrypton <tiendat1446@gmail.com>
2025-05-13 10:14:53 +0700

    lab01 hello world

---
+++ lab_01/ex02_01.py
@@ -0,0 +1,3 @@
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+print("Wellcome to The World,", ten, "! Bạn đã sống sót ở thế giới này", tuoi, "năm rồi à. Kinh nhờ!")
---
+++ lab_01/ex02_03.py
@@ -0,0 +1,1 @@
+so = int(input("Nhập số nguyên coi!:"))
